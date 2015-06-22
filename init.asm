9ebc2dc91ca05c965dfcbe94a5d511a0aed0dd30
tolis24 <apospana@ee.duth.gr>
2015-06-22 03:53:44 +0300

    initialization of peg

--- init.asm
+++ init.asm
@@ -17,4 +17,117 @@
 	addi 2,2,0x10
 	sw   2,0,0x0	#x=0 y=7
 	lui  2,0,0x4
+	sw   2,0,0x0	#x=1 y=0
+	addi 2,2,0x10	
+	sw   2,0,0x0	#x=1 y=1
+	addi 2,2,0x10
+	sw   2,0,0x0	#x=1 y=2
+	addi 2,2,0x12
+	sw   2,0,0x0	#x=1 y=3
+	addi 2,2,0x10	
+	sw   2,0,0x0	#x=1 y=4
+	addi 2,2,0x10
+	sw   2,0,0x0	#x=1 y=5
+	addi 2,2,0xe
+	sw   2,0,0x0	#x=1 y=6
+	addi 2,2,0x10
+	sw   2,0,0x0	#x=1 y=7
+	lui  2,0,0x8
+	sw   2,0,0x0	#x=2 y=0
+	addi 2,2,0x10
+	sw   2,0,0x0	#x=2 y=1
+	addi 2,2,0x10
+	sw   2,0,0x0	#x=2 y=2
+	addi 2,2,0x12
+	sw   2,0,0x0	#x=2 y=3
+	addi 2,2,0x10
+	sw   2,0,0x0	#x=2 y=4
+	addi 2,2,0x10
+	sw   2,0,0x0	#x=2 y=5
+	addi 2,2,0xe
+	sw   2,0,0x0	#x=2 y=6
+	addi 2,2,0x10
+	sw   2,0,0x0	#x=2 y=7
+	lui  2,0,0xc
+	sw   2,0,0x0	#x=3 y=0
+	addi 2,2,0x12
+	sw   2,0,0x0	#x=3 y=1
+	addi 2,2,0x10
+	sw   2,0,0x0	#x=3 y=2
+	addi 2,2,0x10
+	sw   2,0,0x0	#x=3 y=3
+	addi 2,2,0x10
+	sw   2,0,0x0	#x=3 y=4
+	addi 2,2,0x10
+	sw   2,0,0x0	#x=3 y=5
+	addi 2,2,0x10
+	sw   2,0,0x0	#x=3 y=6
+	addi 2,2,0x10
+	sw   2,0,0x0	#x=3 y=7
+	lui  2,0,0x10
+	sw   2,0,0x0	#x=4 y=0
+	addi 2,2,0x12
+	sw   2,0,0x0	#x=4 y=1
+	addi 2,2,0x10
+	sw   2,0,0x0	#x=4 y=2
+	addi 2,2,0x10
+	sw   2,0,0x0	#x=4 y=3
+	addi 2,2,0xf
+	sw   2,0,0x0	#x=4 y=4
+	addi 2,2,0x11
+	sw   2,0,0x0	#x=4 y=5
+	addi 2,2,0x10
+	sw   2,0,0x0	#x=4 y=6
+	addi 2,2,0x10
+	sw   2,0,0x0	#x=4 y=7
+	lui  2,0,0x14
+	sw   2,0,0x0	#x=5 y=0
+	addi 2,2,0x12
+	sw   2,0,0x0	#x=5 y=1
+	addi 2,2,0x10
+	sw   2,0,0x0	#x=5 y=2
+	addi 2,2,0x10
+	sw   2,0,0x0	#x=5 y=3
+	addi 2,2,0x10
+	sw   2,0,0x0	#x=5 y=4
+	addi 2,2,0x10
+	sw   2,0,0x0	#x=5 y=5
+	addi 2,2,0x10
+	sw   2,0,0x0	#x=5 y=6
+	addi 2,2,0x10
+	sw   2,0,0x0	#x=5 y=7
+	lui  2,0,0x18
+	sw   2,0,0x0	#x=6 y=0
+	addi 2,2,0x10
+	sw   2,0,0x0	#x=6 y=1
+	addi 2,2,0x10
+	sw   2,0,0x0	#x=6 y=2
+	addi 2,2,0x12
+	sw   2,0,0x0	#x=6 y=3
+	addi 2,2,0x10
+	sw   2,0,0x0	#x=6 y=4
+	addi 2,2,0x10
+	sw   2,0,0x0	#x=6 y=5
+	addi 2,2,0xe
+	sw   2,0,0x0	#x=6 y=6
+	addi 2,2,0x10
+	sw   2,0,0x0	#x=6 y=7
+	lui  2,0,0x1c
+	sw   2,0,0x0	#x=7 y=0
+	addi 2,2,0x10
+	sw   2,0,0x0	#x=7 y=1
+	addi 2,2,0x10
+	sw   2,0,0x0	#x=7 y=2
+	addi 2,2,0x12
+	sw   2,0,0x0	#x=7 y=3
+	addi 2,2,0x10
+	sw   2,0,0x0	#x=7 y=4
+	addi 2,2,0x10
+	sw   2,0,0x0	#x=7 y=5
+	addi 2,2,0xe
+	sw   2,0,0x0	#x=7 y=6
+	addi 2,2,0x10
+	sw   2,0,0x0	#x=7 y=7
+	
+	
 	
