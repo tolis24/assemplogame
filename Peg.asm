a5536a29f0025f38932e21c37bcd52d13389a9bb
amfitritonas <amfitritonas@gmail.com>
2015-06-23 18:53:52 +0300

    Let's Start (Change FB Address)

---
+++ Peg.asm
@@ -0,0 +1,71 @@
+			addi 	1,0,0x2 	#set register1 the value to send to fb
+			addi 	2,0,-64 	# set r2 to count for the loop (i=-64)
+loop:			sw 		1,0,0		# send value of r1 to fb
+			addi	1,1,0x10	# go to the next tile
+			addi	2,2,0x1		# increase r2(i) by one
+			beq		0,2,loop	
+			lui 	1,0x0
+			sw 	1,0,0  	# 0,0,0
+			addi	1,1,0x10
+			sw 	1,0,0  	# 0,1,0
+			addi	1,1,0x3f	
+			addi	1,1,1	
+			sw 	1,0,0  	# 0,5,0
+			addi	1,1,0x10
+			sw 	1,0,0  	# 0,6,0
+			lui 	1,0x80
+			sw 	1,0,0  	# 1,0,0
+			addi 	1,1,0x10
+			sw 	1,0,0  	# 1,1,0
+			addi	1,1,0x3f	
+			addi	1,1,1
+			sw 	1,0,0  	# 1,5,0
+			addi 	1,1,0x10
+			sw 	1,0,0  	# 1,6,0
+			lui 	1,0x280
+			sw 	1,0,0  	# 5,0,0
+			addi 	1,1,0x10
+			sw 	1,0,0  	# 5,1,0
+			addi	1,1,0x3f	
+			addi	1,1,1
+			sw 	1,0,0  	# 5,5,0
+			addi 	1,1,0x10
+			sw 	1,0,0  	# 5,6,0
+			addi 	1,1,0x20
+			sw 	1,0,0  	# 6,0,0
+			addi 	1,1,0x10
+			sw 	1,0,0  	# 6,1,0
+			addi	1,1,0x3f	
+			addi	1,1,1
+			sw 	1,0,0  	# 6,5,0
+			addi 	1,1,0x10
+			sw 	1,0,0  	# 6,6,0
+			addi 	1,1,0x20
+			sw 	1,0,0  	#7,0,0
+			addi 	2,0,-7
+line:			addi	1,1,0x10
+			sw 	1,0,0  	#7,1-7,0
+			addi 	2,2,1
+			beq 	0,2,line
+			lui 	4,0x80
+			lui	1,0x40
+			addi 	1,1,0x30
+			sw 	1,0,0  	#0,7,0
+			addi 	2,0,-7
+column:			add	1,1,4
+			sw 	1,0,0  	#1-7,7,0
+			addi 	2,2,1
+			beq 	0,2,column
+			lui   	1,0x180
+			addi	1,1,0x31
+			sw 	1,0,0  	#3,3,1
+			
+			
+			
+			
+			
+			
+			
+			# loupa ateleiwti
+			addi 	3,0,3
+infinite:			beq       0,3,infinite
